918df5960a18293d9da0ce1eb85805e72bf139f7
GiulianoMaPe <giulianomape.1806.14@gmail.com>
2026-07-08 18:27:08 -0500

    Fix: Subir imagen a supabase

--- src/app/comerciante/publicar.tsx
+++ src/app/comerciante/publicar.tsx
@@ -47,7 +47,7 @@ export default function PublicarDonacion() {
     }
 
     let resultado = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      mediaTypes: ImagePicker.MediaType.Images,
       allowsEditing: true,
       aspect: [4, 3],
       quality: 0.7,
